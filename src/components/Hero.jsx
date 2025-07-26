9bcecdb30c64ce94e0806f053a42667337192a22
Yash-0006 <yaswanthgodavarthi0006@gmail.com>
2025-07-26 20:16:40 +0530

    final

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import { images } from '../assets/images/images'
 
 const Hero = () => {
   return (
-    <div className='relative flex items-center justify-center h-[90vh] max-w-6xl'>
+    <div className='relative flex items-center justify-center h-[90vh] max-w-9xl'>
 
         <div className='mb-40 mr-25 md:mr-90 md:mt-35 lg:mt-25 lg:mr-150 z-1'>
             <div className='flex justify-between items-center bg-neutral-800 text-white text-sm font-light w-63 md:w-100 lg:w-150'>
@@ -22,7 +22,7 @@ const Hero = () => {
             </div>
             <div className='bg-neutral-900 w-63 h-40 md:w-100 md:h-60  lg:w-150 lg:h-93'></div>
         </div>
-        <div className='absolute z-10 flex flex-col gap-10 md:ml-30 lg:ml-30  items-start sm:flex-row justify-center sm:items-center'>
+        <div className='absolute z-10 flex flex-col gap-10 md:ml-15 lg:ml-30  items-start sm:flex-row justify-center sm:items-center'>
             <div className='leading-[0.9]' >
                 <p className='text-xl lg:text-3xl font-extrabold text-neutral-500'>I'm</p>
                 <p className='stroke-text font-extrabold text-white text-6xl md:text-[6rem] lg:text-[10rem]' >Yaswanth</p>
